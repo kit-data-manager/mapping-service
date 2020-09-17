b22a7b94c0897bc1ba980bb3e245dcb74dc4c371
Andreas Pfeil <pfeil@Andreass-iMac.fritz.box>
2020-09-17 18:34:52 +0200

    Receive and map json records using Handlebars.
    There are issues:
    - see the TODO note for the handlebar helper about string quotation
    - result is not yet stored on disk
    - it is not yet ingested into elasticsearch

--- src/main/java/edu/kit/datamanager/indexer/RecordMessageHandler.java
+++ src/main/java/edu/kit/datamanager/indexer/RecordMessageHandler.java
@@ -15,13 +15,35 @@
  */
 package edu.kit.datamanager.indexer;
 
+import edu.kit.datamanager.clients.SimpleServiceClient;
 import edu.kit.datamanager.entities.messaging.BasicMessage;
 import edu.kit.datamanager.messaging.client.handler.IMessageHandler;
 import edu.kit.datamanager.messaging.client.util.MessageHandlerUtils;
+import edu.kit.datamanager.util.FilenameUtils;
+import javassist.expr.Instanceof;
 import edu.kit.datamanager.indexer.consumer.IConsumerEngine;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import com.github.jknack.handlebars.Context;
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.helper.StringHelpers;
+import com.github.jknack.handlebars.internal.lang3.CharSequenceUtils;
+
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,31 +55,105 @@ public class RecordMessageHandler implements IMessageHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(RecordMessageHandler.class);
 
+    private Handlebars hb;
+
     @Autowired
     private IConsumerEngine consumer;
+
+    RecordMessageHandler() {
+        this.hb = new Handlebars();
+        this.hb.registerHelper("maybeStringify", new Helper<Object>() {
+            /**
+             * This helper will add quotation marks to strings that can not be converted to numbers.
+             * Not that this is a workaround. A json string may contain a string that contains a number.
+             * TODO Ideally, if the json has quotation marks before that, we want to preserve them.
+             */
+            public CharSequence apply(Object o, Options options) {
+                if (! (o instanceof String)) {
+                    System.out.println("It is not a string!");
+                }
+                String thing = (String) o;
+                boolean isInteger = false;
+                boolean isFloat = false;
+                try {
+                    Integer.parseInt(thing);
+                    isInteger = true;
+                } catch (Exception e) {
+                    isInteger = false;
+                }
+                try {
+                    Float.parseFloat(thing);
+                    isFloat = true;
+                } catch (Exception e) {
+                    isFloat = false;
+                }
+                boolean isNumber = isInteger || isFloat;
+                boolean isObject = thing.length() > 0 && thing.charAt(0) == '{';
+                boolean isString = !isNumber && !isObject;
+                if (isString) {
+                    thing = String.format("\"%s\"", thing);
+                }
+                return new Handlebars.SafeString(thing);
+            }
+        });
+    }
 
     @Override
     public RESULT handle(BasicMessage message) {
         LOG.debug("Successfully received message with routing key {}.", message.getRoutingKey());
         // guards which decide to reject early
         // TODO fix message receiving, ideally without casting.
-        //if (message.getEntityName() != "pidrecord") {
-        //    LOG.debug("Reject message: Entity name was {}", message.getEntityName());
-        //    return RESULT.REJECTED;
-        //}
+        // if (message.getEntityName() != "pidrecord") {
+        // LOG.debug("Reject message: Entity name was {}", message.getEntityName());
+        // return RESULT.REJECTED;
+        // }
         if (!MessageHandlerUtils.isAddressed(this.getHandlerIdentifier(), message)) {
             LOG.debug("Reject message: Not addressed correctly");
             return RESULT.REJECTED;
         }
-        LOG.debug("This message is for me!");
+        LOG.debug("This message is for me: {}", message);
 
-        // 1. process using gemma-plugin
-        String record_url = message.getMetadata().get("resolvingUrl");
-        // resolve, convert using gemma
+        // 1. Download record
+        Optional<String> record_json = this.downloadResource(message.getMetadata().get("resolvingUrl"));
+        if (record_json.isEmpty()) {
+            LOG.debug("No JSON was received.");
+            return RESULT.FAILED;
+        }
+        LOG.debug("Received JSON record: {}", record_json.get());
+        // Optional<String> filename =
+        // this.pidToFilename(message.getMetadata().get("pid"));
+        // Optional<String> filename = this.pidToFilename("/tmp/hallo/test");
+        // if (filename.isEmpty()) {
+        // LOG.debug("Could not extract filename to store json. Abort.");
+        // return RESULT.FAILED;
+        // }
+        // Optional<Path> filepath = this.storeAsTempFile(record_json.get(),
+        // filename.get());
 
-        // 2. hand over data to elasticsearch (a consumer impl)
-        // Note that gemma can do this, maybe just make a java API for gemma that allows this?
-        //  -> In this case, consumers may not be necessary anymore.
+        // 2. Verify record (do not trust anyone)
+        // - verify schema
+        // - verify that pid resolves to url that was given?
+
+        // 3. Map record to elasticsearch json file
+        String elastic_json = "";
+        try {
+            Template template = hb.compile("mytemplate");
+            JSONObject jsonData = new JSONObject(record_json.get());
+            Context c = Context
+                .newBuilder(template)
+                .combine(jsonData.toMap())
+                .build();
+            elastic_json = template.apply(c);
+        } catch (Exception e) {
+            LOG.debug("Could not build and apply template. {}", e);
+            return RESULT.FAILED;  // TODO when to use FAILED and when REJECTED?
+        }
+        LOG.debug("Result for elasticsearch is: {}", elastic_json);
+        // TODO maybe check the types in here, afterwards, instead of helper function?
+        // TODO store on disk
+
+        // 4. Send to elasticsearch (consumer impl?)
+
         return RESULT.SUCCEEDED;
     }
 
@@ -65,5 +161,77 @@ public class RecordMessageHandler implements IMessageHandler {
     public boolean configure() {
         // no configuration necessary
         return true;
+    }
+
+    /**
+     * Transforms a given PID to a filename where the record with this PID can be stored.
+     * @param pid the given PID.
+     * @return if the PID was not empty or null, it will return a filename. Empty otherwise.
+     */
+    private Optional<String> pidToFilename(String pid) {
+        if (pid == null || pid.isEmpty()) {
+            return Optional.empty();
+        }
+        pid = pid.replace('/', '_');
+        pid = pid.replace('\\', '_');
+        pid = pid.replace('|', '_');
+        pid = pid.replace('.', '_');
+        pid = pid.replace(':', '_');
+        pid = pid.replace(',', '_');
+        pid = pid.replace('%', '_');
+        pid = pid.replace('!', '_');
+        pid = pid.replace('$', '_');
+        String filename = String.format("{}{}.json", "record", pid);
+        return Optional.of(filename);
+    }
+
+    /**
+     * Downloads the file behind the given URI and returns its content as a string.
+     * 
+     * @param resourceURL the given URI
+     * @return the content of the file (the body of the response) as a string. null
+     *         if a problem occurred.
+     */
+    private Optional<String> downloadResource(String resourceURL) {
+        URL url;
+        try {
+            url = new URL(resourceURL);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+        String theResource = SimpleServiceClient
+            .create(url.toString())
+            .accept(MediaType.APPLICATION_JSON)
+            // TODO I think this function actually might throw an exception. Handle?
+            .getResource(String.class);
+        return Optional.of(theResource);
+    }
+
+    /**
+     * Stores the given content in a file with the given name within the systems
+     * temporary directory.
+     * 
+     * @param content  the given content
+     * @param filename the given filename
+     * @return the absolute path to the stored file on success.
+     */
+    private Optional<Path> storeAsTempFile(String content, String filename) {
+        if (content == null || filename == null) {
+            LOG.error("Did not receive any resource in the response body. Unable to continue.");
+            return Optional.empty();
+        }
+
+        File directory = Paths.get(System.getProperty("java.io.tmpdir")).toFile();
+        File target = new File(directory, filename);
+        Path target_path = Paths.get(target.getAbsolutePath());
+        try {
+            LOG.trace("Writing data resource to temporary file {}.", target_path);
+            FileOutputStream out = new FileOutputStream(target);
+            out.write(content.getBytes());
+            out.close();
+        } catch (Exception e) {
+            LOG.error("Failed to write data resource to temporary file.", e);
+        }
+        return Optional.ofNullable(target_path);
     }
 }
