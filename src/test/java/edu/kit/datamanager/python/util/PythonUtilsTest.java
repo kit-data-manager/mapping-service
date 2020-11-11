2f5c598e307d23addbfc0590b0598d7aa9efa53b
Volker Hartmann <volker.hartmann@kit.edu>
2020-11-11 08:33:14 +0100

    Add code from kitdm-python-connector.

---
+++ src/test/java/edu/kit/datamanager/python/util/PythonUtilsTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.kit.datamanager.python.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Torridity
+ */
+public class PythonUtilsTest {
+
+    //@Test
+    //public void testTrue() {
+    //    FirstClass instance = new FirstClass();
+    //    Assert.assertTrue(instance.testMe(true));
+    //}
+
+    //@Test
+    //public void testFalse() {
+    //    FirstClass instance = new FirstClass();
+    //    Assert.assertFalse(instance.testMe(false));
+    //}
+
+    // TODO inspiration for writing tests:
+    //  public static void main(String[] args){
+    //
+    //    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    //
+    //    System.out.println(PythonUtils.run("/Users/jejkal/anaconda/bin/python", "/Users/jejkal/NetBeansProjects/KITDM-2.0/gemma/retrieve_response.py", System.out, System.err, "./testmd"));
+    //
+    //    //System.out.println("PRINTE " + output.toString());
+    //  }
+}
