c787c51056d48bf7b3dc2da06f07010a080b4194
Maximilian Inckmann <maximilian.inckmann@kit.edu>
2022-09-21 09:38:06 +0200

    Added/modified some tests

--- src/test/java/edu/kit/datamanager/mappingservice/MappingServiceApplicationTest.java
+++ src/test/java/edu/kit/datamanager/mappingservice/MappingServiceApplicationTest.java
@@ -1,14 +1,26 @@
-//package edu.kit.datamanager.mappingservice;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@SpringBootTest
-//class MappingServiceApplicationTest {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//}
+/*
+ * Copyright 2022 Karlsruhe Institute of Technology.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.kit.datamanager.mappingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingServiceApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
