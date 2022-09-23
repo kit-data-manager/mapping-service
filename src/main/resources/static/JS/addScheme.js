fe65bb2a85b9bec557f7cd3f94dee47ecdbdb0fa
Maximilian Inckmann <maximilian.inckmann@kit.edu>
2022-09-23 17:28:31 +0200

    minor optimizations

--- src/main/resources/static/JS/addScheme.js
+++ src/main/resources/static/JS/addScheme.js
@@ -71,7 +71,7 @@ function load() {
 function addACL() {
     const sid = document.getElementById("sid").value
     const permission = document.getElementById("permission").value
-    if (aclEdit != null) {
+    if (aclEdit != null && sid !== acl[0].sid) {
         deleteACL(aclEdit)
         aclEdit = null
     }
@@ -118,10 +118,24 @@ function addACLElement(index, sid, permission) {
 
 function editACL(index) {
     console.log("Editing ACL " + acl[index])
+    document.getElementById("addACLButton").click()
     document.getElementById("sid").value = acl[index].sid
     document.getElementById("permission").value = acl[index].permission
-    document.getElementById("addACLButton").click()
+    showSID(true)
     aclEdit = index
+}
+
+function showSID(edit){
+    if(isEdit && edit) {
+        console.log("Disabling input!")
+        document.getElementById("sid").disabled = true
+        document.getElementById("sid").ariaReadOnly = "true"
+    } else {
+        console.log("Enabling input!")
+        if(!edit) document.getElementById("sid").value = ""
+        document.getElementById("sid").disabled = false
+        document.getElementById("sid").ariaReadOnly = "false"
+    }
 }
 
 function deleteACL(index) {
