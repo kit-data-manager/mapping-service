514b15aade68bbef181efbe94e51f90d5962e049
Maximilian Inckmann <maximilian.inckmann@kit.edu>
2022-09-29 09:59:39 +0200

    Fixed Config

--- src/test/java/edu/kit/datamanager/mappingservice/rest/impl/MappingAdministrationControllerTest.java
+++ src/test/java/edu/kit/datamanager/mappingservice/rest/impl/MappingAdministrationControllerTest.java
@@ -88,7 +88,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         TransactionalTestExecutionListener.class,
         WithSecurityContextTestExecutionListener.class})
 @ActiveProfiles("test")
-@TestPropertySource(properties = {"server.port=41300"})
+@TestPropertySource(properties = {"server.port=41300", "mapping-service.python-location=/usr/bin/python3", "mapping-service.mappings-location=/tmp/mapping-service/"})
 public class MappingAdministrationControllerTest {
 
     private final static String TEMP_DIR_4_MAPPING = "/tmp/mapping-service/";
