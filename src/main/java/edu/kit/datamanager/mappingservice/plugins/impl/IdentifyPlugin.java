09a058d89c6e9c05e40c02e4266c8d8b34465b4e
Thomas Jejkal <thomas.jejkal@kit.edu>
2023-06-13 12:11:54 +0200

    Added GemmaPlugin to default plugins, fixed echo.bat to produce proper output

--- src/main/java/edu/kit/datamanager/mappingservice/plugins/impl/IdentifyPlugin.java
+++ src/main/java/edu/kit/datamanager/mappingservice/plugins/impl/IdentifyPlugin.java
@@ -1,6 +1,17 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright 2023 Karlsruhe Institute of Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package edu.kit.datamanager.mappingservice.plugins.impl;
 
@@ -8,7 +19,6 @@ import edu.kit.datamanager.mappingservice.plugins.IMappingPlugin;
 import edu.kit.datamanager.mappingservice.plugins.MappingPluginException;
 import edu.kit.datamanager.mappingservice.plugins.MappingPluginState;
 import edu.kit.datamanager.mappingservice.util.ShellRunnerUtil;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
